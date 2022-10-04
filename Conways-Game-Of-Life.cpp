a13907f8c238b8696c2a9853be0eff02973ba2ec
Darcy Wilson <26544407+VoxAndrews@users.noreply.github.com>
2022-10-04 15:05:19 +0800

    Added GLFW and GLM Submodules

---
+++ Conways-Game-Of-Life.cpp
@@ -0,0 +1,12 @@
+﻿// Conways-Game-Of-Life.cpp : Defines the entry point for the application.
+//
+
+#include "Conways-Game-Of-Life.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
